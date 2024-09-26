0ab45e441a81482dfe1954a3399845d19dad5383
elzorroartico <gilabert.alexander@gmail.com>
2024-09-26 15:22:38 +0200

    im learning

---
+++ Snark/src/Stats.tsx
@@ -0,0 +1,21 @@
+import  NavFooter  from './components/NavFooter/NavFooter'
+import './Stats.css'
+import './App.css'
+
+type StatPropps = {
+    title:? string;
+    xstats: number[];
+    ystats: number[];
+    xtitle:? string;
+    ytitle:? string; 
+}
+
+const Statsfield = ({, man , lägger, in}) => {
+
+
+    return
+        //Här gör du koden för snyghet
+        <div>   
+        
+        </div>;
+} ;
