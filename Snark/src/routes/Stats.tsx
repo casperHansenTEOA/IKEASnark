541ff72bb00d4d4ed312e33040bab66d2635d94e
elzorroartico <gilabert.alexander@gmail.com>
2024-09-26 16:47:09 +0200

    Finished stats page

--- Snark/src/routes/Stats.tsx
+++ Snark/src/routes/Stats.tsx
@@ -1,21 +1,29 @@
-import  NavFooter  from '../components/NavFooter/NavFooter'
-import './Stats.css'
+import NavFooter from "../components/NavFooter/NavFooter";
+import "./Stats.css";
 // import './App.css'
-import Card from '../components/Card/Card';
-import exp from 'constants';
+import Card from "../components/Card/Card";
+import exp from "constants";
+import { NavLink } from "react-router-dom";
 
-function Stats(){
-//const för saker? 
-return(
-    <div>
-     <Card startImage='src/assets/TimeInBed.png'/>
-     <Card startImage='src/assets/Asleep.png'/>
-     <Card startImage='src/assets/AsleepAfter.png'/>
 
+function Stats() {
+  //const för saker?
+  return (
+    <div className="wrapper">
+      <div className="horizontal-buttons">
+        <NavLink to= "/manual">
+            <Card startImage="src/assets/Manual.png" />
+        </NavLink>
+        <NavLink to="/brasida">
+        <Card startImage="src/assets/BraKnapp.png" />
+
+        </NavLink>
+      </div>
+      <Card startImage="src/assets/TimeInBed.png" />
+      <Card startImage="src/assets/Asleep.png" />
+      <Card startImage="src/assets/AsleepAfter.png" />
     </div>
-);
-    
-
+  );
 }
 
 export default Stats;
@@ -25,15 +33,14 @@ export default Stats;
 //     xstats: number[];
 //     ystats: number[];
 //     xtitle:? string;
-//     ytitle:? string; 
+//     ytitle:? string;
 // }
 
 // const Statsfield = ({variabler, man , lägger, in}) => {
 
-
 //     return
 //         //Här gör du koden för snyghet
-//         <div>   
-        
+//         <div>
+
 //         </div>;
 // } ;
