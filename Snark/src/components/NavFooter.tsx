dce43451adc3890000a12b5ee684ba51e6815485
casper-hansen <60180409+casperHansenTheEnemyOfAustria@users.noreply.github.com>
2024-09-26 13:06:18 +0200

    Created footer and inserted into app

---
+++ Snark/src/components/NavFooter.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NavFooter.css'; // Assuming you have some CSS for styling
+
+const NavFooter: React.FC = () => {
+    return (
+        <footer className="nav-footer">
+            <nav>
+                <ul>
+                    <li><Link to="/home">Home</Link></li>
+                    <li><Link to="/stats">About</Link></li>
+                    <li><Link to="/profile">Services</Link></li>
+                </ul>
+            </nav>
+        </footer>
+    );
+};
+
+export default NavFooter;
