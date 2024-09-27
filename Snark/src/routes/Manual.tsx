ec4a71a099e86b8f2688131f04a6e2773124f53a
elzorroartico <gilabert.alexander@gmail.com>
2024-09-27 11:25:23 +0200

    added code for arrow but no arrow

--- Snark/src/routes/Manual.tsx
+++ Snark/src/routes/Manual.tsx
@@ -1,9 +1,11 @@
 import "./Manual.css";
 import Card from "../components/Card/Card";
+import { FaArrowLeft } from "react-icons/fa";
 
 function Manual() {
   return (
     <div id="overlay">
+      <FaArrowLeft id="ArrowLeft" />
       <div className="horizontal-cards">
         <Card startImage="src/assets/manualVErktyg.png" />
         <Card startImage="src/assets/manualVErktyg.png" />
