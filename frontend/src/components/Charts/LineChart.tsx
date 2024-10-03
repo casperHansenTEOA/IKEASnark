41b617ba480f2eb8cdf06b6c39d569c0d04013b8
Simon Westlin Green <simon.green@live.se>
2024-10-03 10:36:53 +0200

    Working build pls

--- frontend/src/components/Charts/LineChart.tsx
+++ frontend/src/components/Charts/LineChart.tsx
@@ -1,7 +1,17 @@
 // components/Charts/LineChart.js
 import { Line } from "react-chartjs-2";
 
-function LineChart({ chartData }) {
+import { BubbleDataPoint, ChartData, Point } from "chart.js";
+
+interface LineChartProps {
+  chartData: ChartData<
+    "line",
+    (number | [number, number] | Point | BubbleDataPoint | null)[],
+    unknown
+  >;
+}
+
+function LineChart({ chartData }: LineChartProps) {
   return (
     <div className="chart-container">
       <h2 style={{ textAlign: "center" }}>Line Chart</h2>
